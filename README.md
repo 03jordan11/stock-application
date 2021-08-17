c64fdd79489325cd5611d9056da158f41e64c72f
Jordan <jordanburnes@gmail.com>
2021-08-17 18:19:13 -0500

    added main.py for gui and updated Readme with todo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # An application for stock market analysis
+
+# To do
+- Make Dev/Prod a command line argument
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import tkinter as tk
+
 #if no settings.json, initial startup
+#Intro page to tell you what you will need
+#ask for API Key
+#ask for Time zone
+#ask if you would like to set up a development environment
+##for this would create 2 folders and set Env variable to DEV
